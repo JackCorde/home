0ad974061991bdf2ec2a5c609473b7ed017b6ec3
Jack <JackCorderod@outlook.com>
2023-02-28 12:41:11 -0600

    hola

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
     <div class="container-fluid" style="background-image:url(img/fonn.jpg);"></div>
     <center>
-        <h1>Home Segurity</h1>
+        <br><br><br><h1>Home Segurity</h1>
         <br><br><br><h3>SunLinkSystems LTD</h3>
         <br><br><br>
         <p>
